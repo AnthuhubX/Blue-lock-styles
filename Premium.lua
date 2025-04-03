f2d63cb836e48c9fba98d9b0cbc627f2d50746d2
AnthuhubX <grazialmeida140190@gmail.com>
2025-04-03 19:37:11 -0300

    Update Premium.lua

--- Premium.lua
+++ Premium.lua
@@ -24,7 +24,7 @@ local whitelist = {
     ["Ramo_akh1"] = true,
     ["Soulshubfan"] = true,
     ["Nugifihack"] = true,
-    ["mason64132"] = true
+    ["mason72219"] = true
 }
 
 if not whitelist[player.Name] then
